3ab2f6abf94696ee77ffd8d66a8f3e7a410b4c6b
VictoriaVMari <victoriavmari@gmail.com>
2024-03-12 07:14:02 -0300

    Movimiento jugador
    Le agregue codigo al jugador para poder moverlo.

---
+++ Proyecto Motores 1/Assets/Codigo/ControlesJugador.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem; 
+
+public class ControlesJugador : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        S
+    }
+
+    void OnMove(InputValue MovementValue)
+    {
+        Vector2 movementVector = MovementValue.Get<Vector2>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
