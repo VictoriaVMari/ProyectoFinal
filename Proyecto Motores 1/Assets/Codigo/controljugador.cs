af2897811e41a628671eb01a7a4bf32dc6efe185
VictoriaVMari <victoriavmari@gmail.com>
2024-03-15 14:05:45 -0300

    Eliminacion controles de jugador
    Surgio un error en el codigo de controles que no permitia jugar, por lo que lo elimine y lo hice desde cero

--- Proyecto Motores 1/Assets/Codigo/controljugador.cs
+++ Proyecto Motores 1/Assets/Codigo/controljugador.cs
@@ -1,20 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem; 
 
-public class ControlesJugador : MonoBehaviour
+public class controljugador : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        S
-    }
-
-    void OnMove(InputValue MovementValue)
-    {
-        Vector2 movementVector = MovementValue.Get<Vector2>();
-
+        
     }
 
     // Update is called once per frame
